a97234677e8a1ff15aac70324331a1b813d79009
Andr3xDev <achavarropp@gmail.com>
2025-07-08 10:41:43 -0500

    add: documentation

---
+++ accessing-data-mongodb/src/test/java/com/example/accessing_data_mongodb/AccessingDataMongodbApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.accessing_data_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class AccessingDataMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
